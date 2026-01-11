1f84712b368d135a7d4d263390aa717d27b8a36a
Shahzeb M <shahzeb.masroor@gmail.com>
2026-01-10 20:15:44 -0500

    Create WN11-AU-000010.ps1

---
+++ WN11-AU-000010.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    The system must be configured to audit Account Logon – Credential Validation failures
+
+
+.NOTES
+    Author          : Shahzeb Masroor
+    LinkedIn        : linkedin.com/in/shahzebmasroor/
+    GitHub          : github.com/shahm-1
+    Date Created    : 2026-01-10
+    Last Modified   : 2026-01-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-AU-000010
+
+.TESTED ON
+    Date(s) Tested  : 2026-01-10
+    Tested By       : Shahzeb Masroor
+    Systems Tested  : Stiggy-Situation
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN11-AU-000010).ps1 
+#>
+auditpol /set /subcategory:"Credential Validation" /failure:enable
